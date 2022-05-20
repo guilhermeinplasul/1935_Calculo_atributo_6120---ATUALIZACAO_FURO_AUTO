d082c79211cc23d4dd2a634763f222f1a62c6bc1
guilhermeinplasul <101659439+guilhermeinplasul@users.noreply.github.com>
2022-05-20 16:38:07 -0300

    Update stable.sql
    Atributos que devem ser alterados, de acordo com o 6217: 6230, 5560, 6270, 6183 e 6186.
    Caso atributo 12 = 'AGULHA', Atributo 6230 deve ser preenchido com a medida 0,7 mm.

--- stable.sql
+++ stable.sql
@@ -1,158 +1,282 @@
-declare 
-  v_empresa number := :p_1;
-  v_produto estitem.codigo%type   := :p_2;
-  v_versao  pcpversao.versao%type := :p_3;
-  v_retorno pcpficha.valor_padrao%type;
+/*
+  Ticket do ANDREI ENGENHARIA
+  Atributo 6217 - DETALHES DA BOCA DA EMBALAGEM
+  Comando 1935 - Calculo do atributo 6120
   
-  v_atributo6217 number:= 0;
-  
-begin 
-   select nvl(valor_padrao,0)
-     into v_atributo6217
-     from pcpficha 
-    where empresa = v_empresa 
-      and produto = v_produto 
-      and versao = v_versao
-      and atributo = 6217;
-  
-   -- Tamanho do Picote Facilitador 6183
-    if  v_atributo6217 in(1,2,13) then
-              update pcpficha set valor_padrao = 8 
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6183;
-      elsif v_atributo6217 = 3 then
-              update pcpficha set valor_padrao = 11 
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6183;
-      elsif v_atributo6217 = 18 then
-              update pcpficha set valor_padrao = 7 
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6183;                
-      elsif v_atributo6217 in (4) then
-              update pcpficha set valor_padrao = '3.5 A 9.5'
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6183;
-      elsif v_atributo6217 in (5,9,12) then
-              update pcpficha set valor_padrao = 10
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6183;
-      elsif v_atributo6217 in (6,7) then
-              update pcpficha set valor_padrao = 9.5
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6183;
-      elsif v_atributo6217 = 8 then
-              update pcpficha set valor_padrao = 12 
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6183;
-      elsif v_atributo6217 in (10,15) then
-              update pcpficha set valor_padrao = 5 
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6183;
-    end if;
-   
-       -- Distancia da borda do furo até o início do picote 6270
-    if  v_atributo6217 in(1,2,3,4,5,9,12,13,15) then
-              update pcpficha set valor_padrao = 5 
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6270;
-      elsif v_atributo6217 in (6,7,10) then
-              update pcpficha set valor_padrao = 6
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6270;
-      elsif v_atributo6217 in (18) then
-              update pcpficha set valor_padrao = 7
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6270;
-      elsif v_atributo6217 = 8 then
-              update pcpficha set valor_padrao = 3 
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6270;
-     
-    end if;
-  
-       -- Diâmetro do furo 6230
-    if  v_atributo6217 in(3,4,5,8,9,13,14) then
-              update pcpficha set valor_padrao = 17 
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6230;
-      elsif v_atributo6217 in (6,7) then
-              update pcpficha set valor_padrao = 13
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6230;
-      elsif v_atributo6217 in (1,12) then
-              update pcpficha set valor_padrao = 20
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6230;
-      elsif v_atributo6217 in (2,10,15,18) then
-              update pcpficha set valor_padrao = 15
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6230;
-       
-    end if;
+  Atributos que devem ser alterados, de acordo com o 6217: 6230, 5560, 6270, 6183 e 6186
+*/
+
+DECLARE
+
+    v_empresa      NUMBER := :p_1;
+    v_produto      estitem.codigo%TYPE := :p_2;
+    v_versao       pcpversao.versao%TYPE := :p_3;
+    v_retorno      pcpficha.valor_padrao%TYPE;
     
-        -- Posição do picote facilitador 6186
-    if  v_atributo6217 in(1,2,3,4,8,10,12,13,18, 15) then
-              update pcpficha set valor_padrao = 'ALINHADO COM O CENTRO DO FURO'
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6186;
-      elsif v_atributo6217 in (5,9) then
-              update pcpficha set valor_padrao = 'DESLOCADO DO FURO PARA FORA DA EMBALAGEM'
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6186;
-      elsif v_atributo6217 in (6,7) then
-              update pcpficha set valor_padrao = 'DESLOCADO DO FURO PARA O CENTRO DA EMBALAGEM'
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 6186;
- 
-    end if;
-  
-       -- Possui picote facilitador 5560
-    if  v_atributo6217 in(15,18) then
-              update pcpficha set valor_padrao = 'SIM'
-               where empresa = v_empresa 
-                 and produto = v_produto 
-                 and versao = v_versao
-                and atributo = 5560;
-    end if;  
-  
-  v_retorno := v_atributo6217;
-  :p_4 :=	v_retorno;
-end;
+    v_atributo6217 NUMBER := 0;
+    v_atributo12   VARCHAR(200);
+    
+    
+BEGIN
+
+    -- PEGA ATRIB. 6217
+    SELECT Nvl(valor_padrao, 0)
+    INTO   v_atributo6217
+    FROM   pcpficha
+    WHERE  empresa = v_empresa
+           AND produto = v_produto
+           AND versao = v_versao
+           AND atributo = 6217;
+           
+    -- PEGA ATRIB. 12
+    SELECT Nvl(valor_padrao, 0)
+    INTO   v_atributo12
+    FROM   pcpficha
+    WHERE  empresa = v_empresa
+           AND produto = v_produto
+           AND versao = v_versao
+           AND atributo = 12;
+    
+    -- 01 Atributo 6217 DETALHES_BOCA_EMBALAGEM | 6183 TAMANHO_PICOTE_FACILITADOR
+    IF v_atributo6217 IN( 1, 2, 13 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 8
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6183;
+             
+    ELSIF v_atributo6217 = 3 THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 11
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6183;
+             
+    ELSIF v_atributo6217 = 18 THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 7
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6183;
+             
+    ELSIF v_atributo6217 IN ( 4 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = '3.5 A 9.5'
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6183;
+             
+    ELSIF v_atributo6217 IN ( 5, 9, 12 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 10
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6183;
+             
+    ELSIF v_atributo6217 IN ( 6, 7 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = '9.5'
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6183;
+             
+    ELSIF v_atributo6217 = 8 THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 12
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6183;
+             
+    ELSIF v_atributo6217 IN ( 10, 15 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 5
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6183;
+             
+     -- Essa verificacao LIMPA o atributo caso algum 'lixo' tenha persistido
+    ELSIF v_atributo6217 NOT IN (1,2,3,4,5,6,7,8,9,10,12,13,15,18) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = ''
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6183;
+             
+    END IF;
+
+    -- 02 Atributo 6217 DETALHES_BOCA_EMBALAGEM | 6270 AFASTAMENTO_FURO_PICOTE_FACILITADOR
+    IF v_atributo6217 IN( 1, 2, 3, 4, 5, 9, 12, 13, 15 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 5
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6270;
+             
+    ELSIF v_atributo6217 IN ( 6, 7, 10 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 6
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6270;
+             
+    ELSIF v_atributo6217 IN ( 18 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 7
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6270;
+             
+    ELSIF v_atributo6217 = 8 THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 3
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6270;
+             
+    -- Essa verificacao LIMPA o atributo caso algum 'lixo' tenha persistido
+    ELSIF v_atributo6217 NOT IN (1,2,3,4,5,6,7,8,9,10,12,13,15,18) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = ''
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6270;  
+             
+    END IF;
+
+    -- 03 Atributo 6217 DETALHES_BOCA_EMBALAGEM | 6230 DIAMETRO_FURO
+    IF v_atributo6217 IN ( 3, 4, 5, 8, 9, 13, 14 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 17
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6230;
+             
+    ELSIF v_atributo6217 IN (6,7) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 13
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6230;
+             
+    ELSIF v_atributo6217 IN (1,12) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 20
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6230;
+             
+    ELSIF v_atributo6217 IN (2,10,15,16,17,18,19,20) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 15
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6230;
+             
+    -- Essa verificacao LIMPA o atributo caso algum 'lixo' tenha persistido
+    ELSIF v_atributo6217 NOT IN (1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = ''
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6230;         
+    
+    END IF;
+
+    -- 04 Atributo 6217 DETALHES_BOCA_EMBALAGEM | 6186 POSICAO_PICOTE_FACILITADOR 
+    IF v_atributo6217 IN( 1, 2, 3, 4, 8, 10, 12, 13, 15, 18 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 'ALINHADO COM O CENTRO DO FURO'
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6186;
+             
+    ELSIF v_atributo6217 IN ( 5, 9 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 'DESLOCADO DO FURO PARA FORA DA EMBALAGEM'
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6186;
+             
+    ELSIF v_atributo6217 IN ( 6, 7 ) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 'DESLOCADO DO FURO PARA O CENTRO DA EMBALAGEM'
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6186;
+             
+    -- Essa verificacao LIMPA o atributo caso algum 'lixo' tenha persistido
+    ELSIF v_atributo6217 NOT IN (1,2,3,4,5,6,7,8,9,10,12,13,15,18) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = ''
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6186;
+    
+    END IF;
+
+    -- 05 Atributo 6217 DETALHES_BOCA_EMBALAGEM | 5560 POSSUI_PICOTE_FACILITADOR 
+    IF v_atributo6217 IN (15,18) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = 'SIM'
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 5560;
+             
+    -- Essa verificacao LIMPA o atributo caso algum 'lixo' tenha persistido
+    ELSIF v_atributo6217 NOT IN (15,18) THEN
+      UPDATE pcpficha
+      SET    valor_padrao = ''
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 5560;
+    
+    END IF;
+    
+     -- 06 Atributo 12 TIPO_FURACAO
+    IF v_atributo12 = 'AGULHA' THEN
+      UPDATE pcpficha
+      SET    valor_padrao = '0.7'
+      WHERE  empresa = v_empresa
+             AND produto = v_produto
+             AND versao = v_versao
+             AND atributo = 6230;
+             
+    END IF;
+
+    /*
+    Dbms_Output.Put_Line ('5560 POSSUI_PICOTE_FACILITADOR : ' || v_atributo5560);
+    Dbms_Output.Put_Line ('6186 POSICAO_PICOTE_FACILITADOR: ' || v_atributo6186);
+    Dbms_Output.Put_Line ('6183 TAMANHO_PICOTE_FACILITADOR: ' || v_atributo6183);
+    Dbms_Output.Put_Line ('6230 DIAMETRO_FURO             : ' || v_atributo6230);
+    Dbms_Output.Put_Line ('6270 AFASTAMENTO_FURO_PICOTE_FACILITADOR: ' || v_atributo6270);
+    */
+
+    v_retorno := v_atributo6217;
+    :p_4 := v_retorno;
+    
+END; 
